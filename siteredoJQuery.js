9c75e6dcd4e9ed4a650e4b7218d0b0ca77d710c7
kuhnm3 <melanie.kuhn11@gmail.com>
2016-04-15 18:37:38 -0400

    Update siteredoJQuery.js

--- siteredoJQuery.js
+++ siteredoJQuery.js
@@ -16,3 +16,16 @@ $(document).ready(function(e) {
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   $('#animate').stop()
 };
+
+var isMobile = { 
+Android: function() { return navigator.userAgent.match(/Android/i); }, 
+BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); }, 
+iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, 
+Opera: function() { return navigator.userAgent.match(/Opera Mini/i); }, 
+Windows: function() { return navigator.userAgent.match(/IEMobile/i); }, 
+any: function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+$(function($) {
+    if (!isMobile.any())
+    $('#animate').stop('spaceship');
+});
