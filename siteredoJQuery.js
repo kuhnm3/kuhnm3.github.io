9b1684a55de3fb56c616da92919a114af9cddf9d
kuhnm3 <melanie.kuhn11@gmail.com>
2016-04-18 16:15:44 -0400

    Update siteredoJQuery.js

--- siteredoJQuery.js
+++ siteredoJQuery.js
@@ -3,29 +3,17 @@ $(document).on('click', '.menu-btn', function(){
 })
 
 $(document).ready(function(e) {
-    var width = "+=" + 
-    $(document).width();
+
+  var width = "+=" + 
+  $(document).width();
+
+  
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    ;
+  }else{
     $("#animate").animate({
-    left: width,
-    bottom:width,  
-  }, 4000, function() {
-    // Animation complete.
-  });
-});
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('#animate').stop()
-};
-
-var isMobile = { 
-Android: function() { return navigator.userAgent.match(/Android/i); }, 
-BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); }, 
-iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, 
-Opera: function() { return navigator.userAgent.match(/Opera Mini/i); }, 
-Windows: function() { return navigator.userAgent.match(/IEMobile/i); }, 
-any: function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
-
-$(function($) {
-    if (!isMobile.any())
-    $('#animate').stop('spaceship');
+      left: width,
+      bottom:width, 
+    }, 4000);
+  }
 });
