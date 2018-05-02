21702bd682ee5f89bbf2800a021c453865daa670
kuhnm3 <melanie.kuhn11@gmail.com>
2018-05-01 22:17:00 -0400

    Update new_site.js

--- new_site.js
+++ new_site.js
@@ -8,3 +8,40 @@ function scrollDown() {
 }
 
 scrollDown();
+
+function changeHeightNavBar() {
+
+$(window).scroll(function() {
+        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+        var os = $('.hero-unit').offset().top; // pixels to the top of div1
+        var ht = $('.hero-unit').height(); // height of div1 in pixels
+        //if you have scrolled past div
+        if (scroll > os + ht) {
+            $('.nav-section-container').addClass('scrolled');
+        }
+ });
+
+}
+changeHeightNavBar();
+
+function mobileNav() {
+
+    $(".hamburger-button-container").click(function() {
+        $(this).css("display", "none");
+        $('.main-nav').css("display", "flex");
+        $('.x-out-container').css("display", "flex");
+        $('.x-out').css("display", "flex");
+        $('.nav-section-container').addClass("nav-open");
+    });
+
+    $(".x-out-container").click(function() {
+        $(this).css("display", "none");
+        $('.main-nav').css("display", "none");
+        $('.hamburger-button-container').css("display", "flex");
+        $('.nav-section-container').removeClass("nav-open");
+    });
+
+
+}
+
+ mobileNav();
